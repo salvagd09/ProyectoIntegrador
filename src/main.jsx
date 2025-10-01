f6f81ab5fb68e8b484af282aef7c7e6cc0c1cba9
LuisAS7 <luioangelito7@gmail.com>
2025-10-01 13:06:00 -0500

    Parte visual del Menú

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
+import '@fortawesome/fontawesome-free/css/all.min.css';
 import Principal from "./Principal";
 import "./index.css";
 createRoot(document.getElementById("root")).render(
